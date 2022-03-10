9298e92bf2b79a1bd642de82d67606006cfaf1b8
ethacochran <ethan.macrae.cochran@gmail.com>
2022-03-10 00:44:09 +0000

    added calc rotate code

--- index.js
+++ index.js
@@ -8,7 +8,17 @@
 
 (function(window, _) {
     window.gamz = window.gamz || {
-      numz: {/* other code */},
+      numz: {getAngleDegrees(pointA, pointB) {const distanceX = pointB.x - pointA.x 
+        const distanceY = pointB.y - pointA.y
+        const radians = Math.atan2(distanceY, distanceX)
+        const degrees = radians*180/Math.PI},
+        degreesToRadians(degrees) {
+          return degrees * Math.PI/180
+        },
+        radiansToDegrees(radians){
+          return radians * 180/Math.PI
+        }
+      },
       phyz: {
         /**
          * Returns an Object with basic properties utilized in a 
